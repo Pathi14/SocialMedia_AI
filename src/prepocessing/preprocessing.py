633e80597aaaa54cd897c75c2bb25c06085194c4
Paul-HenryN <ngpaulhenry@gmail.com>
2025-02-09 12:49:13 +0100

    feat: setup analysis api endpoint

---
+++ src/prepocessing/preprocessing.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import re
+import pickle
+from pathlib import Path
+
+vectorizer = pickle.load(
+    open(Path(__file__).parent.parent / "models/vectorizer.pkl", "rb")
+)
+
+
+# 🔹 Fonction de nettoyage du texte
+def clean_text(text):
+    text = text.lower()  # Minuscule
+    text = re.sub(r"[^\w\s]", "", text)  # Suppression des caractères spéciaux
+    return text
+
+
+def preprocess(texts: list[str]):
+    cleaned = [clean_text(text) for text in texts]
+    return vectorizer.transform(cleaned)
